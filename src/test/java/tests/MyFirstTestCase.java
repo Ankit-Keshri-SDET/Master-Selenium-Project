7a519866939662a4030deb4a9864c2b5802e2a60
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-07-10 19:15:11 +0530

    Commit 15 -> Added Products Array and Updated TC - HardCoding removed

--- src/test/java/tests/MyFirstTestCase.java
+++ src/test/java/tests/MyFirstTestCase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import objects.BillingAddress;
+import objects.Products;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import base.BaseTest;
@@ -9,23 +10,26 @@ import pages.CheckoutPage;
 import pages.HomePage;
 import pages.StorePage;
 import utils.JacksonUtils;
+
 import java.io.IOException;
 
 public class MyFirstTestCase extends BaseTest {
     @Test
     public void guestCheckoutUsingDirectBankTransfer() throws InterruptedException, IOException {
-
         // JacksonBind API - Made Method Generic to be used by any Java POJO Class
         BillingAddress billingAddress = JacksonUtils.deserializeJson("myBillingAddress.json", BillingAddress.class);
+
+        // Product Object for passing from json and class
+        Products product = new Products(1215);
         HomePage hp = new HomePage(driver).loadURL();
         StorePage sp = hp.clickStoreMenuLink();
         sp.searchProduct("Blue");
         Thread.sleep(1500);
         Assert.assertTrue(sp.getTitle().contains("Blue"), "Incorrect Search Results ...");
-        sp.clickAddToCartButton("Blue Shoes");
+        sp.clickAddToCartButton(product.getName());
         Thread.sleep(500);
         CartPage cp = sp.clickOnViewCartLink();
-        Assert.assertEquals(cp.getProductNameOnCartPage(), "Blue Shoes", "Incorrect product added to Cart ...");
+        Assert.assertEquals(cp.getProductNameOnCartPage(), product.getName(), "Incorrect product added to Cart ...");
         CheckoutPage ccp = cp.clickOnCheckoutBtn()
                 .enterBillingDetails(billingAddress)
                 .placeOrder();
@@ -33,20 +37,22 @@ public class MyFirstTestCase extends BaseTest {
         Assert.assertEquals(ccp.getNotice(), "Thank you. Your order has been received.");
     }
 
-    @Test
+    @Test()
     public void loginAndCheckoutUsingDirectBankTransfer() throws InterruptedException, IOException {
         BillingAddress billingAddress = JacksonUtils.deserializeJson("myBillingAddress.json", BillingAddress.class);
+        Products product = new Products(1215);
         StorePage sp = new HomePage(driver)
                 .loadURL()
                 .clickStoreMenuLink()
                 .searchProduct("Blue");
         Thread.sleep(1500);
         Assert.assertTrue(sp.getTitle().contains("Blue"), "Incorrect Search Results ...");
-        sp.clickAddToCartButton("Blue Shoes");
+        sp.clickAddToCartButton(product.getName());
         Thread.sleep(500);
         CartPage cp = sp.clickOnViewCartLink();
-        Assert.assertEquals(cp.getProductNameOnCartPage(), "Blue Shoes", "Incorrect product added to Cart ...");
+        Assert.assertEquals(cp.getProductNameOnCartPage(), product.getName(), "Incorrect product added to Cart ...");
         CheckoutPage ccp = cp.clickOnCheckoutBtn()
+                .login("smokeT@gmail.com", "Pass12345")
                 .enterBillingDetails(billingAddress)
                 .placeOrder();
         Thread.sleep(2000);
