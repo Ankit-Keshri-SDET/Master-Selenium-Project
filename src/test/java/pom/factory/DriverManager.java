27fe19c4151129faa4c3e469b229ebc61aac5532
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-07-13 23:09:36 +0530

    Commit 26 -> Started Firefox Browser - Multiple Browser Support

--- src/test/java/pom/factory/DriverManager.java
+++ src/test/java/pom/factory/DriverManager.java
@@ -3,12 +3,16 @@ package pom.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
 
     public WebDriver initDriver() {
-        WebDriverManager.chromedriver().cachePath("Drivers").setup();
-        WebDriver driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().cachePath("Drivers").setup();
+//        WebDriver driver = new ChromeDriver();
+
+        // Section 17 - Multiple Browser Support
+        WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         return driver;
     }
