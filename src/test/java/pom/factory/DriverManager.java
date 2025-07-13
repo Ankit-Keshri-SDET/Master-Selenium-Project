8cf28797dc4a5cb21c2f90b451b4ea241f6b0633
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-07-14 01:02:17 +0530

    Commit 28 -> Created ENUM To handle CHROME,FIREFOX Browser Constants

--- src/test/java/pom/factory/DriverManager.java
+++ src/test/java/pom/factory/DriverManager.java
@@ -1,21 +1,25 @@
 package pom.factory;
 
+import constants.BrowserType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static constants.BrowserType.CHROME;
+import static constants.BrowserType.FIREFOX;
 
 public class DriverManager {
 
     public WebDriver initDriver() {
         WebDriver driver;
         String browser = System.getProperty("browser");
-        switch (browser) {
-            case "chrome":
+        switch (BrowserType.valueOf(browser)) {
+            case CHROME:
                 WebDriverManager.chromedriver().cachePath("Drivers/").setup();
                 driver = new ChromeDriver();
                 break;
-            case "firefox":
+            case FIREFOX:
                 WebDriverManager.firefoxdriver().cachePath("Drivers/").setup();
                 driver = new FirefoxDriver();
                 break;
